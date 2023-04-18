444d7516056bf7b7c648db6a745b33b6a4afbb47
odkfu <odkfu1@gmail.com>
2023-04-19 01:51:50 +0900

    feat: set init

---
+++ src/main/java/com/example/visualizechildrenoftherune/VisualizeChildrenOfTheRuneApplication.java
@@ -0,0 +1,13 @@
+package com.example.visualizechildrenoftherune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizeChildrenOfTheRuneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualizeChildrenOfTheRuneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/visualizechildrenoftherune/VisualizeChildrenOfTheRuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.visualizechildrenoftherune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizeChildrenOfTheRuneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
